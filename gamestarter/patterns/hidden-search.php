4d02026fa2951355044a801a5d0896521c21e70c
senol <senoldilya@gmail.com>
2024-05-08 09:42:30 +0200

    repo kopiert

---
+++ gamestarter/patterns/hidden-search.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Search
+ * Slug: Gamestarter/hidden-search
+ * Inserter: no
+ */
+?>
+
+<!-- wp:search {"label":"<?php echo esc_attr_x( 'Search', 'search form label', 'Gamestarter' ); ?>","showLabel":false,"buttonText":"<?php echo esc_attr_x( 'Search', 'search button text', 'Gamestarter' ); ?>","fontSize":"medium"} /-->
---
+++ gamestarter/patterns/page-home-business.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Business home
+ * Slug: Gamestarter/page-home-business
+ * Categories: Gamestarter_page
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ */
+?>
+
+<!-- wp:pattern {"slug":"Gamestarter/banner-hero"} /-->
+<!-- wp:pattern {"slug":"Gamestarter/text-feature-grid-3-col"} /-->
+<!-- wp:pattern {"slug":"Gamestarter/text-alternating-images"} /-->
+<!-- wp:pattern {"slug":"Gamestarter/testimonial-centered"} /-->
+<!-- wp:pattern {"slug":"Gamestarter/posts-list"} /-->
+<!-- wp:pattern {"slug":"Gamestarter/cta-subscribe-centered"} /-->
